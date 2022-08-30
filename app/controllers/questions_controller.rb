41ef18fec5e6b1ff636ccc7021fd1b41b62e0a08
houssam chakir <thisischchannel@gmail.com>
2022-08-30 21:42:45 +0100

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+  def home
+  end
+
+  def ask
+  end
+
+  def answer
+    @answer = ""
+    if params[:question] == "I am going to work"
+      @answer = "great!"
+    elsif params[:question].end_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = " I don't care, get dressed and go to work!"
+    end
+    return @answer
+  end
+end
